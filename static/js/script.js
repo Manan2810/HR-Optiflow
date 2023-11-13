d5c202b2e93c6bb03518003545bdfaae02190d22
Manan Mehra <mananmehra330@gmail.com>
2023-11-14 01:11:37 +0530

    some changes made in these codes

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
     let mediaRecorder;
     let recordedChunks = [];
 
+    // Sample questions
+    const questions = [
+        "Please introduce yourself and tell us about your experience.",
+        "What skills do you possess that make you a good fit for this position?"
+    ];
+
+    let currentQuestionIndex = 0;
+
     // Access the camera and start video preview
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then(stream => {
@@ -18,13 +26,26 @@ document.addEventListener('DOMContentLoaded', function () {
             };
 
             mediaRecorder.onstop = () => {
+                const timestamp = new Date().toISOString().replace(/[-T:\.Z]/g, "");
+                const questionNumber = currentQuestionIndex + 1;
+                const questionText = questions[currentQuestionIndex].replace(/\s+/g, "_"); // Remove spaces and replace with underscores
+                const filename = `question${questionNumber}_${questionText}_${timestamp}.webm`;
+
                 const blob = new Blob(recordedChunks, { type: 'video/webm' });
                 const url = URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = 'interview.webm';
-                document.body.appendChild(a);
-                a.click();
+                
+                downloadFile(url, filename).then(() => {
+                    // Move to the next question or finish if all questions have been asked
+                    currentQuestionIndex++;
+
+                    if (currentQuestionIndex < questions.length) {
+                        // If there are more questions, display the next question
+                        showQuestion(questions[currentQuestionIndex]);
+                    } else {
+                        // If all questions are done, you might want to redirect or perform another action
+                        alert("Interview completed!");
+                    }
+                });
             };
         })
         .catch(error => console.error('Error accessing camera:', error));
@@ -43,4 +64,31 @@ document.addEventListener('DOMContentLoaded', function () {
         startRecordingButton.disabled = false;
         stopRecordingButton.disabled = true;
     });
+
+    // Initial display of the first question
+    showQuestion(questions[currentQuestionIndex]);
 });
+
+function showQuestion(question) {
+    // Update the HTML to display the current question
+    const questionDisplay = document.getElementById('question-display');
+    questionDisplay.textContent = question;
+}
+
+function downloadFile(url, filename) {
+    return new Promise((resolve, reject) => {
+        const a = document.createElement('a');
+        a.href = url;
+
+        // Set a unique download filename
+        a.download = filename;
+
+        document.body.appendChild(a);
+        a.click();
+
+        // Remove the temporary anchor element
+        document.body.removeChild(a);
+
+        resolve();
+    });
+}
